b350933555373f3c26f41ea8cc239ca9f4cd65e5
kwonhygge <kitteniso1103@gmail.com>
2020-06-19 14:09:36 +0900

    Added state for cocktailbuilder

--- src/containers/CocktailBuilder/CocktailBuilder.js
+++ src/containers/CocktailBuilder/CocktailBuilder.js
@@ -2,11 +2,21 @@ import React,{Component} from "react";
 import Cocktail from "../../components/Cocktail/Cocktail"
 
 class CocktailBuilder extends Component{
+    state={
+        ingredients:{
+            cherry:false,
+            lemon:false,
+            sourMix:0,
+            cranberry:0,
+            peachLiquor:1
+        }
+    }
+
     render(){
         return(
             <>
-                <Cocktail />
-                <div>Cocktail Builder</div>
+                <Cocktail ingredients={this.state.ingredients}/>
+                <div>Build Controls</div>
             </>
         )
     }
